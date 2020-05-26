4d4a3b4e0a1b29d3afa6d8e40b1d08f2d8e84400
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2020-05-26 22:08:30 +0300

    Pep8

--- Check spreadsheet/check_spreadsheet.py
+++ Check spreadsheet/check_spreadsheet.py
@@ -1,16 +1,18 @@
-import time
-import pickle
-import os.path
 import datetime
-import post_sm
-from dotenv import load_dotenv
-from urlextract import URLExtract
+import os.path
+import pickle
+import time
 from urllib.parse import urlparse
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+
+from dotenv import load_dotenv
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from urlextract import URLExtract
+
+import post_sm
 
 
 def main():
@@ -21,6 +23,7 @@ def main():
         check_spreadsheet(spreadsheet_id, data_range)
         time.sleep(300)
 
+
 def check_spreadsheet(spreadsheet_id, data_range):
     sheet = authorize_spreadsheets_api()
     spreadsheet_data = download_spreadsheet_data(spreadsheet_id, data_range, sheet)
@@ -28,6 +31,7 @@ def check_spreadsheet(spreadsheet_id, data_range):
     todays_pubs = get_today_unposted_publications(spreadsheet_data, week_day)
     if todays_pubs:
         post_pubs(todays_pubs, spreadsheet_id, sheet)
+
 
 def authorize_spreadsheets_api():
     # Code snippet from: https://developers.google.com/sheets/api/quickstart/python
@@ -54,14 +58,16 @@ def authorize_spreadsheets_api():
     sheet = service.spreadsheets()
     return sheet
 
+
 def download_spreadsheet_data(spreadsheet_id, data_range, sheet):
     value_render_option = 'FORMULA'
     result = sheet.values().get(spreadsheetId=spreadsheet_id,
-                                range=data_range, 
+                                range=data_range,
                                 valueRenderOption=value_render_option
                                 ).execute()
     data = result.get('values', [])
     return data
+
 
 def get_today_week_day():
     week_days = [
@@ -71,11 +77,12 @@ def get_today_week_day():
         'четверг',
         'пятница',
         'суббота',
-        'воскресенье',        
+        'воскресенье',
     ]
     today = datetime.datetime.today()
     week_day = week_days[today.weekday()]
     return week_day
+
 
 def get_today_unposted_publications(spreadsheet_data, week_day):
     todays_pubs = []
@@ -86,6 +93,7 @@ def get_today_unposted_publications(spreadsheet_data, week_day):
         if week_day == pub_day.lower() and was_posted.lower() == 'нет':
             todays_pubs.append([*pub_info, line_number])
     return todays_pubs
+
 
 def post_pubs(todays_pubs, spreadsheet_id, sheet):
     now_hour = datetime.datetime.now().hour
@@ -99,21 +107,28 @@ def post_pubs(todays_pubs, spreadsheet_id, sheet):
         if txt_file_name:
             with open(txt_file_name, 'r', encoding='utf-8') as txt_file:
                 text = txt_file.read()
-        
+
         try:
-            if post_vk.lower() == 'да': post_sm.post_vkontakte(img_file_name, text)
-            if post_tg.lower() == 'да': post_sm.post_telegram(img_file_name, text)
-            if post_fb.lower() == 'да': post_sm.post_facebook(img_file_name, text)
+            if post_vk.lower() == 'да':
+                post_sm.post_vkontakte(img_file_name, text)
+            if post_tg.lower() == 'да':
+                post_sm.post_telegram(img_file_name, text)
+            if post_fb.lower() == 'да':
+                post_sm.post_facebook(img_file_name, text)
         finally:
-            if img_file_name: os.remove(img_file_name)
-            if txt_file_name: os.remove(txt_file_name)
+            if img_file_name:
+                os.remove(img_file_name)
+            if txt_file_name:
+                os.remove(txt_file_name)
             update_pub_status(spreadsheet_id, line_number, sheet)
+
 
 def pass_auth_gdrive():
     gauth = GoogleAuth()
-    gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+    gauth.LocalWebserverAuth()  # Creates local webserver and auto handles authentication.
     drive = GoogleDrive(gauth)
     return drive
+
 
 def download_pub_txt(pub_text, drive):
     text_id = get_gdrive_file_id(pub_text)
@@ -123,6 +138,7 @@ def download_pub_txt(pub_text, drive):
         txt_file_name = None
     return txt_file_name
 
+
 def download_pub_img(pub_image, drive):
     img_id = get_gdrive_file_id(pub_image)
     if img_id:
@@ -131,6 +147,7 @@ def download_pub_img(pub_image, drive):
         img_file_name = None
     return img_file_name
 
+
 def get_gdrive_file_id(text):
     urls = URLExtract().find_urls(text)
     if urls:
@@ -138,12 +155,14 @@ def get_gdrive_file_id(text):
         file_id = parsed.query[3:]
         return file_id
 
+
 def download_txt_from_gdrive(text_id, drive):
     file_txt = drive.CreateFile({'id': text_id})
     txt_name = file_txt['title']
     file_name = f'{txt_name}.txt'
-    file_txt.GetContentFile( file_name, mimetype='text/plain')
+    file_txt.GetContentFile(file_name, mimetype='text/plain')
     return file_name
+
 
 def download_img_from_gdrive(image_id, drive):
     file_img = drive.CreateFile({'id': image_id})
@@ -152,6 +171,7 @@ def download_img_from_gdrive(image_id, drive):
     file_img.GetContentFile(image_name)
     return image_name
 
+
 def update_pub_status(spreadsheet_id, line_number, sheet):
     cell_litera = 'H'
     cell_name = f'{cell_litera}{line_number}'
@@ -159,10 +179,11 @@ def update_pub_status(spreadsheet_id, line_number, sheet):
     body = {
         'values': [['да']]
     }
-    result = sheet.values().update(spreadsheetId=spreadsheet_id, 
-        valueInputOption=value_input_option, range=cell_name, 
-        body=body).execute()
-    return
+    result = sheet.values().update(spreadsheetId=spreadsheet_id,
+                                   valueInputOption=value_input_option, range=cell_name,
+                                   body=body).execute()
+    return result
+
 
 if __name__ == '__main__':
     main()
--- Check spreadsheet/post_sm.py
+++ Check spreadsheet/post_sm.py
@@ -1,7 +1,8 @@
 import os
-import vk_api
+
 import requests
 import telegram
+import vk_api
 
 
 def post_vkontakte(image_path, text):
@@ -17,14 +18,16 @@ def post_vkontakte(image_path, text):
         photo_id = vk_upload_photo(upload, image_path, vk_group_id)
         photo_name = f'photo-{vk_group_id}_{photo_id}'
         vk.wall.post(owner_id=f'-{vk_group_id}', attachments=photo_name)
-                
+
+
 def customize_vk_api():
-    vk_access_token = os.getenv('VK_ACCESS_TOKEN')    
+    vk_access_token = os.getenv('VK_ACCESS_TOKEN')
     vk_api_version = '5.101'
     vk_session = vk_api.VkApi(token=vk_access_token, api_version=vk_api_version)
     vk = vk_session.get_api()
     upload = vk_api.VkUpload(vk_session)
     return vk, upload
+
 
 def vk_upload_photo(upload, image_path, vk_group_id):
     vk_album_id = os.getenv('VK_ALBUM_ID')
@@ -36,6 +39,7 @@ def vk_upload_photo(upload, image_path, vk_group_id):
     photo_id = photo_info[0]['id']
     return photo_id
 
+
 def post_telegram(image_path, text):
     tg_bot_token = os.getenv('TG_BOT_TOKEN')
     chat_url = os.getenv('TG_CHAT_URL')
@@ -46,17 +50,18 @@ def post_telegram(image_path, text):
         with open(image_path, 'rb') as photo:
             bot.send_photo(chat_id=chat_url, photo=photo)
 
+
 def post_facebook(image_path, text):
     fb_group_id = os.getenv('FB_GROUP_ID')
     fb_token = os.getenv('FB_TOKEN')
     url = f'https://graph.facebook.com/{fb_group_id}/photos'
-    payload = {'access_token': fb_token,}
+    payload = {'access_token': fb_token}
     if text:
         payload.update({'caption': text})
     if image_path:
         with open(image_path, 'rb') as photo:
-            files = {'file': photo}       
+            files = {'file': photo}
             response = requests.post(url, files=files, params=payload)
     else:
         response = requests.post(url, params=payload)
-    response.raise_for_status()   
+    response.raise_for_status()
